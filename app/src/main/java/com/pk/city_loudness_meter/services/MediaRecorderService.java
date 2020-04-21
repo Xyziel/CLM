2c378bf242701ce571b29c40a472ee04e0977455
cyklop123 <irekbiela@gmail.com>
2020-04-21 13:48:44 +0200

    Add basic sound measurments

--- app/src/main/java/com/pk/city_loudness_meter/services/MediaRecorderService.java
+++ app/src/main/java/com/pk/city_loudness_meter/services/MediaRecorderService.java
@@ -1,10 +1,47 @@
 package com.pk.city_loudness_meter.services;
 
+import android.media.MediaRecorder;
+import android.net.wifi.hotspot2.pps.Credential;
+
 public class MediaRecorderService {
+
+    MediaRecorder recorder;
+
+    public MediaRecorderService()
+    {
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.UNPROCESSED);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_WB);
+        recorder.setOutputFile("/dev/null");
+        try
+        {
+            recorder.prepare();
+        }catch (java.io.IOException ioe) {
+            android.util.Log.e("[MAIN]", "IOException: " + android.util.Log.getStackTraceString(ioe));
+
+        }catch (java.lang.SecurityException e) {
+            android.util.Log.e("[MAIN]", "SecurityException: " + android.util.Log.getStackTraceString(e));
+        }
+        try
+        {
+            recorder.start();
+        }catch (java.lang.SecurityException e) {
+            android.util.Log.e("[MAIN]", "SecurityException: " + android.util.Log.getStackTraceString(e));
+        }
+    }
+
+    protected double getAmplitude() {
+        if (recorder != null)
+            return recorder.getMaxAmplitude();
+        else
+            return 0;
+
+    }
 
     public double getDb()
     {
-        return 0.0;
+        return 20 * Math.log10(getAmplitude());
     }
 
 }
