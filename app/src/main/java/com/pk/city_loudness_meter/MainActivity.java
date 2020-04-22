2f3a2ad242391e8f995af2a95e8c7656e9b2ec18
Mikolaj <mikolajbibro@gmail.com>
2020-04-22 10:35:20 +0200

    new permissions added

--- app/src/main/java/com/pk/city_loudness_meter/MainActivity.java
+++ app/src/main/java/com/pk/city_loudness_meter/MainActivity.java
@@ -1,41 +1,55 @@
 package com.pk.city_loudness_meter;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.LocationServices;
+import com.pk.city_loudness_meter.http.MeasurementRequest;
+import com.pk.city_loudness_meter.services.MediaRecorderService;
+import com.pk.city_loudness_meter.services.LocationService;
+
+import okhttp3.OkHttpClient;
+
 public class MainActivity extends AppCompatActivity {
+
+    private boolean permissionToRecordAccepted = false;
+    private boolean permissionToCourseLocation = false;
+    private boolean permissionToInternet = false;
+    private boolean permissionToAccessNetwork = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         permissionRequest();
 
+        LocationService locationService = new LocationService(LocationServices.getFusedLocationProviderClient(this));
+        MediaRecorderService mediaRecorderService = new MediaRecorderService();
+        MeasurementRequest measurementService = new MeasurementRequest(mediaRecorderService, new OkHttpClient(), locationService);
+
+        measurementService.send();
     }
 
-    private void permissionRequest()
-    {
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED)
-        {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
-        }
+    private void permissionRequest() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO,
+           Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET, Manifest.permission.ACCESS_NETWORK_STATE
+        }, 200);
     }
 
     @Override
-    public void onRequestPermissionsResult(int code, String[] permissions, int[] result)
-    {
-        switch (code)
-        {
-            case 1:
-                if(result.length > 0 && result[0] == PackageManager.PERMISSION_DENIED)
-                {
-                    this.finishAndRemoveTask();
-                }
+    public void onRequestPermissionsResult(int code, String[] permissions, int[] grantResults) {
+        if (code == 200) {
+            permissionToRecordAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+            permissionToCourseLocation = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+            permissionToInternet = grantResults[2] == PackageManager.PERMISSION_GRANTED;
+            permissionToAccessNetwork = grantResults[3] == PackageManager.PERMISSION_GRANTED;
         }
+        if (!permissionToRecordAccepted && !permissionToCourseLocation && !permissionToAccessNetwork && !permissionToInternet)
+            finish();
     }
 }
